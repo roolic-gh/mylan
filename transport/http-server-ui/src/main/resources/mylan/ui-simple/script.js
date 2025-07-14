b4891f68a8e9a329333c8a2b4672359211cef18e
Ruslan Kashapov <roolic.mail@gmail.com>
2025-07-14 16:43:58 +0300

    web-ui: user mgmt related functionality
    - login/logout, change own password, display name
    - user list for admin user
    - create, enable/disable and delete user by admin
    - reset passwords by admin
    
    Signed-off-by: Ruslan Kashapov <roolic.mail@gmail.com>

--- transport/http-server-ui/src/main/resources/mylan/ui-simple/script.js
+++ transport/http-server-ui/src/main/resources/mylan/ui-simple/script.js
@@ -11,8 +11,8 @@ class App {
 
     init() {
         this.ui.init();
-        this.client.getCurrentUser((response) => {
-            this.user.reset(response);
+        this.client.getCurrentUser((currentUser) => {
+            this.user.reset(currentUser);
             this.ui.applyUser(this.user);
         });
     }
@@ -20,29 +20,90 @@ class App {
     login() {
         const creds = {username: byId('username-input').value, password: byId('password-input').value };
         const persist = byId('rememberme-input').checked;
-        console.log("login())", creds, persist);
         this.client.authenticate(creds, (response) => {
             this.client.setToken(response.authToken, persist);
             this.user.reset(response.user);
             this.ui.applyUser(this.user);
+            this.ui.closeModal('login-modal');
             if (response.mustChangePassword) {
                 this.ui.notifyPasswordChangeRequired();
             }
-            this.ui.closeModal('login-modal');
         });
     }
 
     logout() {
-        console.log("logout()");
         // TODO Notify server to cancel session
+        this.client.clearToken();
         this.user.reset({userId: null});
         this.ui.applyUser(this.user);
+        // TODO refresh ui
     }
 
+    updatePassword() {
+        const oldPass = byId('old-password-input').value;
+        const newPass = byId('new-password-input').value;
+        const repPass = byId('repeat-new-password-input').value;
+        if (newPass != repPass) {
+            reportError("Repeat password missmatch.")
+            return;
+        }
+        this.client.updateUserPassword(
+            {userId:this.user.id, oldPassword: oldPass, newPassword: newPass},
+            () => {
+                report("Password updated. Please re-login.");
+                this.ui.closeModal('change-password-modal');
+            });
+    }
+
+    updateProfile() {
+        this.client.updateUserDetails(
+            {userId: this.user.id, displayName: byId('edit-displayname-input').value},
+            (updatedUser) => {
+                this.user.reset(updatedUser);
+                this.ui.applyUser(this.user);
+                this.ui.closeModal('edit-profile-modal');
+            });
+    }
+
+    listUsers() {
+        this.client.getUserList((users) => this.ui.drawUserList(users, this));
+    }
+
+    addUser() {
+        this.client.createUser(
+            {
+                username: byId('new-username-input').value,
+                displayName: byId('new-displayname-input').value,
+                admin: byId('new-isadmin-input').checked
+            },
+            (response) => {
+                this.ui.closeModal('new-user-modal');
+                this.listUsers();
+            });
+    }
+
+    resetUserPassword(user) {
+        this.client.resetUserPassword(user.userId,
+            (response) => report("Password for user @" + user.username + " was reset."));
+    }
+
+    setUserEnabled(user, enabled) {
+        this.client.updateUserStatus(
+            { userId: user.userId, disabled: !enabled },
+            (response) => {
+                report("User @" + user.username + " " + (enabled ? "enabled." : "disabled."));
+                this.listUsers();
+            });
+    }
+
+    deleteUser(user) {
+        // TODO delete confirm dialog
+        this.client.deleteUser( user.userId, () => {
+                report("User @" + user.username + " deleted.");
+                this.listUsers();
+            });
+    }
 }
-
-
-
 
 class MaterialUi{
     selfContext = null;
@@ -53,7 +114,7 @@ class MaterialUi{
 
     init() {
         M.Dropdown.init(document.querySelectorAll(`.dropdown-trigger`), {});
-        M.Modal.init(document.querySelectorAll(`.modal`), {});
+        M.Modal.init(document.querySelectorAll(`.modal`), {onOpenStart: M.updateTextFields});
     }
 
     applyUser(user) {
@@ -62,7 +123,30 @@ class MaterialUi{
         setVisible('users-menu-item', user.isAdmin);
         setVisible('registered-menu-item', !user.isGuest);
         setVisible('login-menu-item', user.isGuest);
-        byId('username').innerHTML = user.displayName;
+        byId('username').textContent = user.displayName;
+        byId('edit-displayname-input').value = user.displayName;
+    }
+
+    drawUserList(users, app) {
+        console.log(users);
+        setTableContentFromTemplate('user-lst-template', users, (user) => [
+            user.userId, user.username, user.displayName,
+            user.admin ? 'admin' : 'user',
+            user.disabled ? 'disabled' : 'active',
+            user.disabled
+                ? [
+                button('Enable', () => app.setUserEnabled(user, true)),
+                button('Delete', () => app.deleteUser(user))
+            ]
+                : [
+                button('Reset Password', () => app.resetUserPassword(user)),
+                button('Disable', () => app.setUserEnabled(user, false))
+            ]
+        ]);
+    }
+
+    openModal(id) {
+        M.Modal.getInstance(byId(id)).close();
     }
 
     closeModal(id) {
@@ -71,6 +155,7 @@ class MaterialUi{
 
     notifyPasswordChangeRequired(){
         M.toast({ html: "Password change is required" });
+        this.openModal('change-password-modal')
     }
 }
 
@@ -95,15 +180,45 @@ class Client {
     }
 
     getCurrentUser(callback) {
-        //        callback({userId: null, username: "guest", displayName: "guest", admin: false});
+        //        callback({userId: 10, username: "admin", displayName: "Admin", admin: true});
         this.request("GET", "/user", callback);
     }
 
     authenticate(creds, callback) {
-        //        callback({user: {userId: 10, username: "test", displayName: "Test", admin: false},
-        //            authToken: "12345", mustChangePassword: true});
         this.request("POST", "/authenticate", callback, creds);
+    }
 
+    updateUserPassword(creds, callback) {
+        this.request("POST", "/user/change-password", callback, creds);
+    }
+
+    resetUserPassword(userId, callback){
+        this.request("POST", "/user/" + userId + "/reset-password", callback);
+    }
+
+    createUser(user, callback) {
+        this.request("POST", "/user/create", callback, user);
+    }
+
+    updateUserDetails(userDeatils, callback) {
+        this.request("PATCH", "/user/details", callback, userDeatils);
+    }
+
+    updateUserStatus(userStatus, callback) {
+        this.request("PATCH", "/user/status", callback, userStatus);
+    }
+
+    deleteUser(userId, callback) {
+        this.request("DELETE", "/user/" + userId, callback);
+    }
+
+    getUserList(callback) {
+        //        callback([
+        //            {userId: 1, username: "admin", displayName: "Admin", admin: true, disabled: false},
+        //            {userId: 2, username: "user1", displayName: "User 1", admin: false, disabled: false},
+        //            {userId: 3, username: "user2", displayName: "User 3", admin: false, disabled: true}
+        //        ]);
+        this.request("GET", "/user/list", callback);
     }
 
     async request(method, path, callback, body) {
@@ -126,46 +241,59 @@ class Client {
             })
         ).then(
             (response) => {
-                if (response.status == 401) {
-                    reportError("Session expired. Please re-login");
+                if (response.status == 401 && this.token != null) {
+                    // authorization header is bein rejected
+                    reportError("Session expired. Please reload & relogin.");
                     this.clearToken();
                     return {};
-                } else {
-                    return response.json()
                 }
+                if (response.status == 204) {
+                    return {};
+                }
+                return response.json();
             }
         ).then(
             (json) => {
                 console.log("fetch response: ", json)
-                if (json.error == null) {
-                    callback(json);
-                } else {
+                if (json.hasOwnProperty('error')) {
                     reportError(json.error);
+                } else {
+                    callback(json);
                 }
-            });
+            }
+        ).catch((error) => reportError(error));
     }
 }
 
 class User {
+    id = null;
     username = null;
     displayName = null;
     isAdmin = false;
     isGuest = true;
 
     reset(serverUser) {
-        //        console.log("user reset: ", serverUser);
         if (serverUser.userId != null) {
+            this.id = serverUser.userId;
             this.username = serverUser.username;
             this.displayName = serverUser.displayName;
             this.isAdmin = serverUser.admin;
             this.isGuest = false;
         } else {
-            this.username = "anon";
+            this.id = null;
+            this.username = "anonimous";
             this.displayName = "Guest";
             this.isAdmin = false;
             this.isGuest = true;
         }
     }
+}
+
+function setTableContentFromTemplate(templateId, items, toCells) {
+    const fragment = byId(templateId).content.cloneNode(true);
+    const tbody = fragment.querySelector('tbody');
+    items.forEach((item) => tbody.append(buildTableRow(toCells(item))));
+    byId('content').replaceChildren(fragment);
 }
 
 function byId(id) {
@@ -176,17 +304,40 @@ function setVisible(id, visible) {
     byId(id).style.display = visible ? '' : 'none';
 }
 
-function buildElement(tag, classes, text, onclick){
-    var elm = document.createElement(tag);
-    elm.setAttribute("class", classes);
-    elm.innerHtml = text;
-    elm.onclick = onclick;
+function buildTable(headers, rows){
+    return elementNode('table', [elementNode('thead', [buildTableRow(headers)]), elementNode('tbody', rows)]);
+}
+
+function buildTableRow(items) {
+    var row = document.createElement('tr');
+    items.forEach((item) => row.append(Array.isArray(item)? elementNode('td', item) : elementText('td', item)));
+    return row;
+}
+
+function elementNode(tag, children) {
+    const elm = document.createElement(tag);
+    children.forEach(((item) => elm.append(item)));
     return elm;
 }
 
+function elementText(tag, text){
+    const elm = document.createElement(tag);
+    elm.textContent = text;
+    return elm;
+}
+
+function button(label, onclick, cls) {
+    const btn = elementText('a', label);
+    btn.setAttribute('class', 'btn-small btn-flat');
+    btn.onclick = onclick;
+    return btn;
+}
+
+function report(message) {
+    M.toast({ html: message, classes: 'blue-grey darken-2' });
+}
 
 function reportError(message) {
     console.log(message);
-    M.toast({ html: message, classes: 'red darken-1' });
+    M.toast({ html: "Error: " + message, classes: 'red darken-2' });
 }
-
